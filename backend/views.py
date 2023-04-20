db3277afc12a947a71c58ac0bd85d0c3ebfe1097
pseuder <iop890520@gmail.com>
2023-04-20 08:19:47 +0800

    first upload

---
+++ backend/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+
+import datetime
+# Create your views here.
+
+def hello_world(request):
+    return render(request, 'hello_world.html', {
+        'current_time': str(datetime.datetime.now()),
+    })
+
+def home(request):
+    post_list = Post.objects.all()
+    return render(request, 'home.html', {
+        'post_list': post_list,
+    })
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    print(pk)
+    return render(request, 'post.html', {'post': post})
